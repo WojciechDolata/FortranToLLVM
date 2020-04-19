898cc25e89b804e3327f02d3fd093d124bdc05b2
WojciechDolata <wojciech.k.dolata@gmail.com>
2020-04-19 17:51:36 +0200

    added grammar files alongside with example f77 program, also now main contains creation of parsing tree

---
+++ src/main/examples/HelloWorld.f
@@ -0,0 +1,13 @@
+      implicit none
+
+      write ( *, '(a)' ) ' '
+      write ( *, '(a)' ) 'HELLO:'
+      write ( *, '(a)' ) '  FORTRAN77 version'
+      write ( *, '(a)' ) '  This is how to say:'
+      write ( *, '(a)' ) ' '
+      write ( *, '(a)' ) '  Hello, world!'
+
+      write ( *, '(a)' ) ' '
+
+      stop
+      end
--- src/main/java/fortran/App.java
+++ src/main/java/fortran/App.java
@@ -1,5 +1,14 @@
 package fortran;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.snt.inmemantlr.GenericParser;
+import org.snt.inmemantlr.listener.DefaultListener;
+import org.snt.inmemantlr.listener.DefaultTreeListener;
+import org.snt.inmemantlr.tree.ParseTree;
+import org.snt.inmemantlr.utils.FileUtils;
+
+import java.io.File;
+
 /**
  * Hello world!
  *
@@ -7,6 +16,31 @@ package fortran;
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        ParserRuleContext ctx = new ParserRuleContext();
+        DefaultTreeListener defaultTreeListener = new DefaultTreeListener();
+        try {
+            File files [] = {
+                    new File("src/main/grammar/Fortran77Lexer.g4"),
+                    new File("src/main/grammar/Fortran77Parser.g4")
+            };
+            GenericParser gp = new GenericParser(files);
+    // 2. load file content into string
+            String s = FileUtils.loadFileContent("src/main/examples/HelloWorld.f");
+    // 3. set listener for checking parse tree elements. Here you could use any ParseTreeListener implementation. The default listener is used per default
+            gp.setListener(defaultTreeListener);
+    // 4. compile Lexer and parser in-memory
+            gp.compile();
+    // 5. parse the string that represents the content of HelloWorld.java
+//            ctx = gp.parse(s, "compilationUnit", GenericParser.CaseSensitiveType.NONE);
+            ctx = gp.parse(s);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println(ctx.toString());
+        ParseTree parseTree = defaultTreeListener.getParseTree();
+        System.out.println(parseTree.toString());
+
+
+
     }
 }

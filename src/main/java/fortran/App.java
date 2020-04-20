a137411c3d245adeed2540cf8725ac06831b1518
WojciechDolata <wojciech.k.dolata@gmail.com>
2020-04-20 18:38:48 +0200

    added ParseTreeCreator which now gives us a tree which we can parse to LLVM, example usage in App.java

--- src/main/java/fortran/App.java
+++ src/main/java/fortran/App.java
@@ -16,31 +16,9 @@ import java.io.File;
 public class App 
 {
     public static void main( String[] args ) {
-        ParserRuleContext ctx = new ParserRuleContext();
-        DefaultTreeListener defaultTreeListener = new DefaultTreeListener();
-        try {
-            File files [] = {
-                    new File("src/main/grammar/Fortran77Lexer.g4"),
-                    new File("src/main/grammar/Fortran77Parser.g4")
-            };
-            GenericParser gp = new GenericParser(files);
-    // 2. load file content into string
-            String s = FileUtils.loadFileContent("src/main/examples/HelloWorld.f");
-    // 3. set listener for checking parse tree elements. Here you could use any ParseTreeListener implementation. The default listener is used per default
-            gp.setListener(defaultTreeListener);
-    // 4. compile Lexer and parser in-memory
-            gp.compile();
-    // 5. parse the string that represents the content of HelloWorld.java
-//            ctx = gp.parse(s, "compilationUnit", GenericParser.CaseSensitiveType.NONE);
-            ctx = gp.parse(s);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-        System.out.println(ctx.toString());
-        ParseTree parseTree = defaultTreeListener.getParseTree();
-        System.out.println(parseTree.toString());
+        ParseTreeCreator parseTreeCreator = new ParseTreeCreator();
+        ParseTree myParseTree = parseTreeCreator.getParseTree("HelloWorld.f");
 
-
-
+        System.out.println(myParseTree.toString());
     }
 }
---
+++ src/main/java/fortran/ParseTreeCreator.java
@@ -0,0 +1,36 @@
+package fortran;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.snt.inmemantlr.GenericParser;
+import org.snt.inmemantlr.listener.DefaultTreeListener;
+import org.snt.inmemantlr.tree.ParseTree;
+import org.snt.inmemantlr.utils.FileUtils;
+
+import java.io.File;
+
+public class ParseTreeCreator {
+
+    private static String lexerPath = "src/main/grammar/Fortran77Lexer.g4";
+    private static String parserPath = "src/main/grammar/Fortran77Parser.g4";
+    private static String baseFilePath = "src/main/examples/";
+
+    public ParseTree getParseTree (String fileName) {
+        ParserRuleContext ctx = new ParserRuleContext();
+        DefaultTreeListener defaultTreeListener = new DefaultTreeListener();
+        try {
+            File[] files = {
+                    new File(lexerPath),
+                    new File(parserPath)
+            };
+            GenericParser gp = new GenericParser(files);
+            String s = FileUtils.loadFileContent(baseFilePath + fileName);
+            gp.setListener(defaultTreeListener);
+            gp.compile();
+//            ctx = gp.parse(s, "compilationUnit", GenericParser.CaseSensitiveType.NONE);
+            ctx = gp.parse(s);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return defaultTreeListener.getParseTree();
+    }
+}

b31d295dfd449784f0e89054c8ad2707471b56ec
WojciechDolata <wojciech.k.dolata@gmail.com>
2020-05-26 12:22:06 +0200

    improved UI

--- src/main/java/ui/HelloFX.java
+++ src/main/java/ui/HelloFX.java
@@ -2,22 +2,33 @@ package ui;
 
 import fortran.CompilerInstance;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
 
 public class HelloFX extends Application {
-
+    @FXML
+    private TextArea codeTextArea;
     private CompilerInstance compilerInstance = new CompilerInstance();
     private Stage stage;
     private File file;
+    private File outputDirectory;
+    private StringProperty program = new SimpleStringProperty();
+
+    public void initialize() {
+        codeTextArea.textProperty().bind(program);
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -36,8 +47,14 @@ public class HelloFX extends Application {
     public void compile() {
         if(file != null) {
             String program = compilerInstance.compileFromFile(file);
+            this.program.setValue(program);
             try {
-                var fileName = file.getParent() + "/" + file.getName().replace(".f", ".llm");
+                String fileName;
+                if(outputDirectory != null) {
+                    fileName = outputDirectory.getAbsolutePath() + "/" + file.getName().replace(".f", ".llm");
+                } else {
+                    fileName = file.getParent() + "/" + file.getName().replace(".f", ".llm");
+                }
                 BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
                 writer.write(program);
                 writer.close();
@@ -49,6 +66,7 @@ public class HelloFX extends Application {
 
     public void pickFile() {
         FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File("."));
         fileChooser.setTitle("Wybierz plik do kompilacji");
         var file = fileChooser.showOpenDialog(stage);
         if(file != null) {
@@ -56,6 +74,17 @@ public class HelloFX extends Application {
         }
     }
 
+    public void pickTargetFolder() {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Wybierz folder docelowy");
+        File defaultDirectory = new File(".");
+        chooser.setInitialDirectory(defaultDirectory);
+
+        var folder = chooser.showDialog(this.stage);
+        if (folder != null) {
+            this.outputDirectory = file;
+        }
+    }
 
 
 }
